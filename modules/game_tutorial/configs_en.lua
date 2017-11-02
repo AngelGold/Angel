6bb054b03f0db6fdaaf9aaa514381aa7256d265d
AngelGold <31815442+AngelGold@users.noreply.github.com>
2017-11-01 17:54:36 -0700

    Patch Game Tutorial
    Tutorial

---
+++ modules/game_tutorial/configs_en.lua
@@ -0,0 +1,40 @@
+tutorialsIndex_en = {"Básico", "Intermedio", "Avanzado", "Profeciones", "Habilidades", "Extra", "Torneos", "Hot Quests"}
+
+tutorialsInfo_en = {
+    {
+        {name = "01. Introdução", text = "1. O Menu Pokemon é onde você chama o seu Pokemon e verifica o seu estado e skills.\n\n2. Sua mochila é onde você irá armazenar seus objetos e seus Pokemons.\n\n3. Informações de seu char, como nível e experiência.", img = "imgs/b1"}, 
+        {name = "02. Controlar Objetos", text = "Para controlar itens , basta clicar e segurar o botão esquerdo do pressionado o mouse e arrastá-lo para onde quiser .", img = "imgs/b2"}, 
+        {name = "03. Chame seu Pokemon", text = "Para chamar seu Pokemon , Pokemon arraste -o para o menu, em seguida, clique no botão direito do seu mouse.", img = "imgs/b3"},
+        {name = "04. Atacando Inimigos", text = "Para atacar um inimigo , basta clicar sobre ele com o botão direito do mouse, então o alvo é marcado e seus Pokémons atacarão o inimigo automaticamente.", img = "imgs/b4"},
+        {name = "05. Usando Skill", text = "Cada skill de seu Pokemon é desbloqueada como você upa seu level e de seu Pokemon. Para usar uma skill basta clicar em seu ícone ( botão esquerdo do mouse ) ou usar atalhos (F1, F2 , F3, F4... ) atalhos também podem ser redefinidos nas hotkeys.", img = "imgs/b5"},
+        {name = "06. Loots", text = "Todos os Pokemons depois de serem derrotados podem ter drops. Para coletá-los basta clicar sobre o Pokémon derrotado com o botão direito do mouse, depois você deve arrastá-los para sua mochila.", img = "imgs/b6"},
+        {name = "07. Catchs de Pokemons", text = "Para capturar um Pokémon , clique em seus Pokeballs vazios ( botão direito do mouse ) e , em seguida, utilizados em um pokemon derrotado ( botão esquerdo do mouse ) .", img = "imgs/b7"}, 
+   },
+    {
+        {name = "01. ??", text = "??.", img = "imgs/Default"},
+        
+    },	
+    {
+        {name = "01. ??", text = "??.", img = "imgs/Default"},
+      
+    },	
+    {
+        {name = "01. ??", text = "??.", img = "imgs/Default"},
+       
+    },	
+    {
+        {name = "01. ??", text = "??.", img = "imgs/Default"},
+		
+    },
+    {
+        {name = "01. ??", text = "??.", img = "imgs/Default"},
+		
+    },		
+    {
+        {name = "01. ??", text = "??.", img = "imgs/Default"},
+    },	
+    {
+        {name = "01. ??", text = "??.", img = "imgs/Default"},
+		
+    },	    
+}
