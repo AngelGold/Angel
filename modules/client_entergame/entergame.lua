2c1aaeb73f6d9882aacd666b5764fb079916ed70
AngelGold <31815442+AngelGold@users.noreply.github.com>
2017-12-04 21:13:05 -0800

    Revert "IP Modificado"
    This reverts commit c61ec34341d1cbe7431f8005a2b6e9ed8e022ccc.

--- modules/client_entergame/entergame.lua
+++ modules/client_entergame/entergame.lua
@@ -134,7 +134,7 @@ function EnterGame.init()
   end
   
   --EnterGame.setUniqueServer(hostName, port, protocolVersion, windowWidth, windowHeight)
-  EnterGame.setUniqueServer('177.71.78.90', 7171, 854, 270, 210)
+  EnterGame.setUniqueServer('177.71.79.5', 7171, 854, 270, 210)
 end
 
 function EnterGame.firstShow()
